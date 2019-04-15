2a1dd5fe1e67a8a45248fa9d4185c4eb963519d4
Cattī Crūdēlēs <wzy960724@gmail.com>
2019-04-15 23:23:45 +0800

    Change JSON format

--- data_parser/main_parser.py
+++ data_parser/main_parser.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from data_parser import population_parser, product_parser, production_building_parser
 
 JSON_INDENT = 2
+VERSION = "Open Beta"
 
 
 def main():
@@ -23,6 +24,7 @@ def main():
                 .contents[3].contents[1].contents[3]("Asset")
         factories = production_building_parser.parse_factories(factory_tags)
         production_buildings = farms + factories
+        production_buildings = {"Version": VERSION, "ProductionBuildings": production_buildings}
         with (output_path / "production_buildings.json").open(mode = "w", encoding = "utf-8") as output_file:
             json.dump(production_buildings, output_file, ensure_ascii = False, indent = JSON_INDENT)
         
@@ -35,6 +37,7 @@ def main():
         workforce_tags = soup.AssetList.Groups.contents[5].Groups.contents[1].Groups.contents[3].Groups.contents[
             1].Groups("Asset")
         workforces = product_parser.parse_workforces(workforce_tags)
+        workforces = {"Version": VERSION, "Workforces": workforces}
         with (output_path / "workforces.json").open(mode = "w", encoding = "utf-8") as output_file:
             json.dump(workforces, output_file, ensure_ascii = False, indent = JSON_INDENT)
         
@@ -45,6 +48,7 @@ def main():
         product_tags = \
             soup.AssetList.Groups.contents[5].Groups.contents[1].Groups.contents[3].Groups.contents[5].Groups("Asset")
         products = product_parser.parse_normal_products(product_tags)
+        products = {"version": VERSION, "Products": products}
         with (output_path / "products.json").open(mode = "w", encoding = "utf-8") as output_file:
             json.dump(products, output_file, ensure_ascii = False, indent = JSON_INDENT)
         
@@ -53,21 +57,24 @@ def main():
         product_categories = product_parser.parse_product_categories(product_category_tags)
         for category in product_categories:
             category['products'] = list()
-        for product in products:
+        for product in products['Products']:
             category_id = product['category']
             for category in product_categories:
                 if category['id'] == category_id:
                     category['products'].append(product['id'])
+        product_categories = {"Version": VERSION, "ProductCategories": product_categories}
         with (output_path / "product_categories.json").open(mode = "w", encoding = "utf-8") as output_file:
             json.dump(product_categories, output_file, ensure_ascii = False, indent = JSON_INDENT)
         
         population_group_tags = soup.AssetList.contents[1].contents[7].contents[1].contents[1]("Asset")
         population_groups = population_parser.parse_population_groups(population_group_tags)
+        population_groups = {"Version": VERSION, "PopulationGroups": population_groups}
         with (output_path / "population_groups.json").open(mode = "w", encoding = "utf-8") as output_file:
             json.dump(population_groups, output_file, ensure_ascii = False, indent = JSON_INDENT)
         
         population_level_tags = soup.AssetList.contents[1].contents[7].contents[1].contents[3]("Asset")
         population_levels = population_parser.parse_population_levels(population_level_tags)
+        population_levels = {"Version": VERSION, "PopulationLevels": population_levels}
         with (output_path / "population_levels.json").open(mode = "w", encoding = "utf-8") as output_file:
             json.dump(population_levels, output_file, ensure_ascii = False, indent = JSON_INDENT)
         
